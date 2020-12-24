8ff407d1135997d54911335bb59f0b77b12593db
wangmz <wangmeizhen@shfas96119.com>
2020-12-24 11:34:52 +0800

    'upLess'

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*入口js*/
+import  React from 'react'
+import  ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import  App from './App'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//将App组件标签渲染到index页面的div上
+ReactDOM.render(<App />,document.getElementById('root'))
